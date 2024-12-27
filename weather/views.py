8bb1e52943ba504b1382450087f9d859ff62feee
rap4iq <almatykz2004@gmail.com>
2024-12-27 20:15:07 +0600

    asdad

--- weather/views.py
+++ weather/views.py
@@ -1,9 +1,49 @@
 import requests
 from django.http import JsonResponse
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login
+from django.contrib.auth.models import User
+from django.contrib import messages
 
 def index(request):
     return render(request, 'weather/index.html')
+
+
+def login_view(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        user = authenticate(request, username=username, password=password)
+        if user:
+            login(request, user)
+            messages.success(request, f"Добро пожаловать, {user.username}!")  # Приветственное сообщение
+            return redirect('index')  # Перенаправление на главную страницу
+        else:
+            messages.error(request, "Неверное имя пользователя или пароль.")  # Ошибка
+            return redirect('login')  # Перенаправление на страницу входа
+
+    return render(request, 'weather/login.html')  # В случае GET-запроса, просто показываем форму входа
+
+def register_view(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        email = request.POST.get('email')
+        password = request.POST.get('password1')
+        confirm_password = request.POST.get('password2')
+
+        if password != confirm_password:
+            messages.error(request, "Пароли не совпадают.")  # Ошибка
+            return redirect('register')
+
+        if User.objects.filter(username=username).exists():
+            messages.error(request, "Пользователь с таким именем уже существует.")  # Ошибка
+            return redirect('register')
+
+        User.objects.create_user(username=username, email=email, password=password)
+        messages.success(request, "Регистрация прошла успешно!")  # Успешное сообщение
+        return redirect('index')  # Перенаправление на главную страницу
+
+    return render(request, 'weather/register.html')
 
 def get_weather(request):
     if request.method == 'GET':
